3952eeedf68588de8b2269a548017058c9f864f9
Andrii <Andrii@MacBook-Air-Sergey-2.local>
2022-02-07 00:56:59 +0200

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,10 +2,18 @@ import flatpickr from "flatpickr";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import "flatpickr/dist/flatpickr.min.css";
 
-const timePicker = document.getElementById('datetime-picker');
-const strartBtn = document.querySelector('[data-start]');
-strartBtn.disabled = true;
+const refs = {
+  DELLAY: 1000,
+  timePicker: document.getElementById('datetime-picker'),
+  strartBtn: document.querySelector('[data-start]'),
+  dDays: document.querySelector('[data-days]'),
+  dHours: document.querySelector('[data-hours]'),
+  dMinutes: document.querySelector('[data-minutes]'),
+  dSeconds: document.querySelector('[data-seconds]')
+}
+refs.strartBtn.disabled = true;
 let chosenDate = null;
+let interval = null;
 
 const options = {
   enableTime: true,
@@ -13,22 +21,58 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {      
-      if (selectedDates[0] - Date.now() < 0) {
+    if (selectedDates[0] - Date.now() < 0) {
           alert("Please choose a date in the future");
           return;
       }
-      chosenDate = selectedDates[0];
-      strartBtn.disabled = false;
+    chosenDate = selectedDates[0];
+    console.log(chosenDate);
+
+      refs.strartBtn.disabled = false;
   },
 };
 
-flatpickr(timePicker, options);
+flatpickr(refs.timePicker, options);
 
-strartBtn.addEventListener('click', onClick)
+refs.strartBtn.addEventListener('click', onClick);
 
 function onClick() {
-    
+  interval = setInterval(renderTimer, refs.DELLAYDELLAY);
+  
 }
+
+function renderTimer() {
+  const oldDate = chosenDate;
+  let timer = null;
+  let math = null;
+  if (oldDate !== chosenDate) {
+    timer = addLeadingZero(convertMs(chosenDate - Date.now()));
+    math = Math.round((chosenDate % Date.now()) / 600)
+    
+  } else {
+    timer = addLeadingZero(convertMs(oldDate - Date.now()));
+    math = Math.round((oldDate % Date.now()) / 600)
+  }
+
+  refs.dDays.textContent = timer.days;
+  refs.dHours.textContent = timer.hours;
+  refs.dMinutes.textContent = timer.minutes;
+  refs.dSeconds.textContent = timer.seconds;
+
+  if (math === 1) {
+    clearInterval(interval);    
+    return;
+  }   
+}
+
+function addLeadingZero({ days, hours, minutes, seconds }) {
+  days = String(days).padStart(2, '0');
+  hours = String(hours).padStart(2, '0');
+  minutes = String(minutes).padStart(2, '0');
+  seconds = String(seconds).padStart(2, '0');
+
+  return { days, hours, minutes, seconds };
+ };
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -49,6 +93,3 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
