5297d0985aa242075788891753b5bd955b29d89b
Andrii <Andrii@MacBook-Air-Sergey-2.local>
2022-02-09 16:42:00 +0200

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,10 +11,11 @@ const form = document.querySelector('.form');
 
 function onSubm(e) {
   e.preventDefault();
-  const delay = form.elements.delay.value;  
+  const startDelay = form.elements.delay.value;  
   const step = form.elements.step.value;
   const amount = form.elements.amount.value;
-  const promises = [];
+  let delay = startDelay;
+  
   
 
   if (amount <= 0) {
@@ -22,22 +23,20 @@ function onSubm(e) {
     return;
   }
   
-  // promises.push({ position: 0, step: delay });
-
-  createPromise(0, delay).then(({ position, delay }) => {
-    Notify.info(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notify.info(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
   
-  for (let i = 1; i <= amount; i += 1) {    
-     createPromise(i, step).then(({ position, delay }) => {
+
+  
+  
+  for (let i = 1; i <= amount; i += 1) {       
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
     Notify.info(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
     Notify.info(`❌ Rejected promise ${position} in ${delay}ms`);
   });
+    delay += step;
+    console.log(i);
   }
 
 }
@@ -48,14 +47,15 @@ form.addEventListener('submit', onSubm);
 
 
 function createPromise(position, delay) {
+  const canFulfill = Math.random() > 0.3;
+
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (delay >= 0) {
-        resolve({ position, delay });
+   setTimeout(() => {
+      if (canFulfill) {
+        resolve({position, delay });
       } else {
         reject({ position, delay });
       }
-    }, delay); 
+    }, delay);
   })
 }
-
